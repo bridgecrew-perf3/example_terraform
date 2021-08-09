80a57a7dd3360c53246395ca3b0b977aec3d2d0c
charlesguo404 <charlesguo.wh@gmail.com>
2021-08-09 23:18:17 +0800

    update trim

--- stringFunctions/trimAndTrimspaceAndTrimprefixAndTrimsuffix.tf
+++ stringFunctions/trimAndTrimspaceAndTrimprefixAndTrimsuffix.tf
@@ -3,8 +3,8 @@
 // trimsuffix 从字符串的结尾删除指定的字符串
 // trimspace 从给定字符串的开头和结尾删除空格
 locals {
-  trimValue = trim("?!hello?!", "!?")
+  trimValue       = trim("?!hello?!", "!?")
   trimprefixValue = trimprefix("helloworld", "hello")
   trimsuffixValue = trimsuffix("helloworld", "world")
-  trimspaceValue = trimspace("  hello\n\n")
+  # trimspaceValue = trimspace("  hello\n\n")
 }
